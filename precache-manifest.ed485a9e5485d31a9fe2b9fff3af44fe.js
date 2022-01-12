b2f6171de247fce97864fc6dd3b00e17ee076452
Sherin Manalel <sherinmanalel@Sherins-MacBook-Pro.local>
2022-01-12 23:26:36 +1100

    Updates

---
+++ precache-manifest.ed485a9e5485d31a9fe2b9fff3af44fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4400b4c79b3ec295b6dd4442dcf1825",
+    "url": "/st-marys/index.html"
+  },
+  {
+    "revision": "c490aa7c71c33ecd55fd",
+    "url": "/st-marys/static/css/main.d7710edf.chunk.css"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/st-marys/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "c490aa7c71c33ecd55fd",
+    "url": "/st-marys/static/js/main.79585c9c.chunk.js"
+  },
+  {
+    "revision": "cfe894877acba37a9299",
+    "url": "/st-marys/static/js/runtime~main.cae569b1.js"
+  }
+]);
